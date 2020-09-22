0e307eba74cda13301e2dc39eceb353492d61af1
necelentano <necelentano@gmail.com>
2020-09-22 14:06:11 +0300

    Added default.json, deleted keys.js and changed server.js

--- config/keys.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  mongoURI: 'mongodb+srv://new-user_84:new-user_84@necelentanocluster.iauzr.mongodb.net/<dbname>?retryWrites=true&w=majority'
-}
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const config = require('config');
 
 const app = express();
 
@@ -8,7 +9,7 @@ const app = express();
 app.use(express.json());
 
 // DB Config
-const db = require('./config/keys').mongoURI;
+const db = config.get('mongoURI');
 
 // Fix deprecation warnings in console
 mongoose.set('useNewUrlParser', true);
