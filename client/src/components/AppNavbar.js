6b31cd9796c419d3375e57962b8ed1553e7b4474
necelentano <necelentano@gmail.com>
2020-09-29 23:37:38 +0300

    Added RegisterModal and Logout components to AppNavbar

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -7,9 +7,11 @@ import {
     NavbarBrand,
     Nav,
     NavItem,
-    NavLink,
     Container
 } from 'reactstrap';
+
+import RegisterModal from './auth/RegisterModal';
+import Logout from './auth/Logout';
 
 class AppNavbar extends Component {
 
@@ -33,9 +35,10 @@ class AppNavbar extends Component {
                         <Collapse isOpen={this.state.isOpen} navbar>
                             <Nav className="ml-auto" navbar>
                                 <NavItem>
-                                    <NavLink href="https://github.com/necelentano">
-                                        Github
-                                    </NavLink>
+                                    <RegisterModal />
+                                </NavItem>
+                                <NavItem>
+                                    <Logout />
                                 </NavItem>
                             </Nav>
                         </Collapse>
