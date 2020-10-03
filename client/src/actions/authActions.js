d2b0b60d4bb6e0c9b29259a2aab8c59cb24edf63
necelentano <necelentano@gmail.com>
2020-10-03 20:00:54 +0300

    Added login action creator

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -7,7 +7,9 @@ import {
     AUTH_ERROR,
     REGISTER_SUCCESS,
     REGISTER_FAIL,
-    LOGOUT_SUCCESS
+    LOGOUT_SUCCESS,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL
 } from '../actions/types';
 
 // Check token & load user
@@ -52,6 +54,33 @@ export const register = ({name, email, password}) => dispatch => {
             dispatch(returnErrors(err.response.data, err.response.status, 'REGISTER_FAIL'));
             dispatch({
                 type: REGISTER_FAIL
+            })
+        })
+}
+
+// Login user
+
+export const login = ({email, password}) => dispatch => {
+
+    //Headers
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    // Request body
+    const body = JSON.stringify({ email, password });
+
+    axios.post('api/auth', body, config)
+        .then(res => dispatch({
+            type: LOGIN_SUCCESS,
+            payload: res.data
+        }))
+        .catch(err => {
+            dispatch(returnErrors(err.response.data, err.response.status, 'LOGIN_FAIL'));
+            dispatch({
+                type: LOGIN_FAIL
             })
         })
 }
