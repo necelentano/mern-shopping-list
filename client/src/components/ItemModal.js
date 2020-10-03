0bcab32941473b5e0ee5809b33510c4502effd9c
necelentano <necelentano@gmail.com>
2020-10-03 21:49:21 +0300

    Render Button with conditional, added PropTypes

--- client/src/components/ItemModal.js
+++ client/src/components/ItemModal.js
@@ -12,12 +12,17 @@ import {
 } from 'reactstrap';
 import { connect } from 'react-redux';
 import { addItem } from '../actions/itemActions';
+import PropTypes from 'prop-types';
 
 class ItemModal extends Component {
 
     state = {
         modal: false,
         name: ''
+    }
+
+    static propTypes = {
+        isAuthenticated: PropTypes.bool
     }
 
     toggle = () => {
@@ -43,13 +48,22 @@ class ItemModal extends Component {
     }
 
     render() {
+
+        const { isAuthenticated } = this.props;
+
+        const addButton = (
+            <Button
+                color="dark"
+                style={{marginBottom: '2rem'}}
+                onClick={this.toggle}>
+                    Add Item 
+            </Button>
+        )
+
         return(
             <Container>
-                <Button
-                    color="dark"
-                    style={{marginBottom: '2rem'}}
-                    onClick={this.toggle}
-                >Add Item</Button>
+                { isAuthenticated ? addButton : <h4 className="mb-3 ml-4">Please log in to manage items</h4> }
+                
                 <Modal isOpen={this.state.modal} toggle={this.toggle}>
                     <ModalHeader toggle={this.toggle}>Add To Shopping List</ModalHeader>
                     <ModalBody>
@@ -69,7 +83,8 @@ class ItemModal extends Component {
 }
 
 const mapStateToProps = state => ({
-    item: state.item
+    item: state.item,
+    isAuthenticated: state.auth.isAuthenticated
 })
 
 export default connect(mapStateToProps, {addItem})(ItemModal);
